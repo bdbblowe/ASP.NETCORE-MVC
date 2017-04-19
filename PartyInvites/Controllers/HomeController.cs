2c356f8636fd1ccfbc1fd6d16b701b29440fb037
bdbblowe <bdbblowe@gmail.com>
2017-04-19 17:24:29 -0500

    now displays responses

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers {
 
@@ -11,8 +13,18 @@ namespace PartyInvites.Controllers {
             return View("MyView");
         }
 
+        [HttpGet]
         public ViewResult RsvpForm() {
             return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+
+        public ViewResult ListResponses() {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
