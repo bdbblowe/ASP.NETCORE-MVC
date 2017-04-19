faef354c59b710bc98ef6c188830e05a0aed4c14
bdbblowe <bdbblowe@gmail.com>
2017-04-19 16:01:28 -0500

    initial commit for PartyInvites

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PartyInvites.Controllers {
+
+    public class HomeController : Controller {
+
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        public ViewResult RsvpForm() {
+            return View();
+        }
+    }
+}
